0f9709ca8c018da6891aaa34da11dc9a1cf24b72
Ricardo Martins Carvalho <ricardomc.ifba@gmail.com>
2021-05-03 07:58:59 -0300

    Rest Template

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.devsuperior.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public RestTemplate getRestTampleta() {
+		
+		return new RestTemplate();
+		
+	}
+	
+}
