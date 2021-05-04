1d939cec9a8bd7d30ee277bba46be4dd54a9fc43
Ricardo Martins Carvalho <ricardomc.ifba@gmail.com>
2021-05-04 07:43:05 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResources.java
@@ -36,11 +36,11 @@ public class WorkerResources {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-		/*try {
+		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}*/
+		}
 
 		logger.info("Port = " + env.getProperty("local.server.port"));
 		Worker obj = repository.findById(id).get();
